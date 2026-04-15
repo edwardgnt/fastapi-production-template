38a010041812b9266e1f1cb43a493cbb303c18ab
edwardgnt <edwardgnt@gmail.com>
2026-04-15 15:08:44 -0700

    Fix Alembic migration and CI issues

--- alembic/env.py
+++ alembic/env.py
@@ -1,10 +1,10 @@
-from logging.config import fileConfig
 import importlib
 import os
+from logging.config import fileConfig
 
-from alembic import context
 from sqlalchemy import engine_from_config, pool
 
+from alembic import context
 from app.db.base import Base
 
 config = context.config
