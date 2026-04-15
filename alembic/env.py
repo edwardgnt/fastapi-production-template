aa0d4ec89f9a0effd4b3e846462f2dadd21a49f1
edwardgnt <edwardgnt@gmail.com>
2026-04-15 14:41:40 -0700

    Fix Alembic linting and CI configuration

--- alembic/env.py
+++ alembic/env.py
@@ -1,11 +1,11 @@
-from logging.config import fileConfig
+import importlib
 import os
+from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config, pool
 
 from alembic import context
 from app.db.base import Base
-import app.models
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
@@ -19,6 +19,8 @@ if database_url:
 # This line sets up loggers basically.
 if config.config_file_name is not None:
     fileConfig(config.config_file_name)
+
+importlib.import_module("app.models.item")
 
 # add your model's MetaData object here
 # for 'autogenerate' support
